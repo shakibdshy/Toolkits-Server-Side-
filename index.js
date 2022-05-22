e06240026f52e1b6b4537dac1fdc517733b6f6a0
Shakib D shy <shakibdshy@gmail.com>
2022-05-23 02:31:33 +0600

    Update Server site to DB

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rl6dv.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const toolsCollection = client.db('ToolKits').collection('tools');
+
+        // Authentication
+        app.post('/login', (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken });
+        });
+     }
+    finally {}
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Running Server');
+})
+
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
