3d7c8e4062d26a5dd86fdcd302ffe2b4e6c85751
Shakib D shy <shakibdshy@gmail.com>
2022-05-29 21:37:28 +0600

    Put Update User

--- index.js
+++ index.js
@@ -177,6 +177,20 @@ async function run() {
         });
 
         //adding more info of an user
+        app.put('/user-info-update/:email', verifyJWT, verifyAdmin, async (req, res) => {
+            const email = req.params.email;
+            const updatedInfo = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: updatedInfo,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            // const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET)
+            res.send({ success: true });
+        });
+
+        //adding more info of an user
         app.put('/user-update/:email', verifyJWT, verifyAdmin, async (req, res) => {
             const email = req.params.email;
             const filter = { email: email };
